bb5fabca487c80bcdb26f88217727933319e792e
umaicode <park.seungchan99@gmail.com>
2023-05-30 10:25:44 +0900

    230525

--- 10장-16.TEMP-F1.cpp
+++ 10장-16.TEMP-F1.cpp
@@ -1,19 +1,20 @@
 // 10장-16.TEMP-F1.CPP  10.5 template 함수
 // template 함수의 선언과 사용 예
+// T min이 안되는 이유 : min()이 내장함수여서 수정하여야 함.
 #include <iostream>
 using namespace std;
-template <class T> 
-T min (T ele1, T ele2){
-    if (ele1 < ele2)
-        return ele1;
-    else
-        return ele2;
+template <class T>
+T minn(T ele1, T ele2) {
+	if (ele1 < ele2)
+		return ele1;
+	else
+		return ele2;
 }
 
-void main (){
-    char c1 = 'W', c2 = 'h';
-    int num1 = 23, num2 = 67;
-    cout << "min(<char>, <char>) = " << min (c1, c2) << endl ;
-    cout << "min(<int>, <int>) = " << min (num1, num2) << endl ;
+void main() {
+	char c1 = 'W', c2 = 'h';
+	int num1 = 23, num2 = 67;
+	cout << "min(<char>, <char>) = " << minn(c1, c2) << endl;
+	cout << "min(<int>, <int>) = " << minn(num1, num2) << endl;
 	system("pause");
 }
--- 10장-18.TEMP-F3.cpp
+++ 10장-18.TEMP-F3.cpp
@@ -3,30 +3,30 @@
 #include <iostream>
 using namespace std;
 
-template <class T> T min (T ele1, T ele2){
-    if (ele1 < ele2)
-        return ele1;
-    else
-        return ele1;
+template <class T> T minn(T ele1, T ele2) {
+	if (ele1 < ele2)
+		return ele1;
+	else
+		return ele2;
 }
-class Number{
-    long num;
+class Number {
+	long num;
 public:
-    Number (long num2) : num (num2) { }
-    long Value (){
-        return num;
-    }
-    /*
-    bool operator < (Number numBer2){
-        return num < numBer2.Value ();
-    }
-    */
-} ;
+	Number(long num2) : num(num2) { }
+	long Value() {
+		return num;
+	}
 
-void main (){
-    Number nn1 = 12;
-    Number nn2 = 18;
-    Number nn3 = min (nn1, nn2);
-    cout << "min(<Number>, <Number>) = " << nn3.Value() << endl ;
+	bool operator < (Number numBer2) {
+		return num < numBer2.Value();
+	}
+
+};
+
+void main() {
+	Number nn1 = 12;
+	Number nn2 = 18;
+	Number nn3 = minn(nn1, nn2);
+	cout << "min(<Number>, <Number>) = " << nn3.Value() << endl;
 	system("pause");
 }
